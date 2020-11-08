14d8637240e9192efb83659ef3031667be9525d1
andreitecsor <andrei.tecsor@gmail.com>
2020-11-08 16:41:58 +0200

    initial commit

---
+++ src/main/java/com/greenhabits/GreenHabitsApplication.java
@@ -0,0 +1,13 @@
+package com.greenhabits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenHabitsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenHabitsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/greenhabits/GreenHabitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenhabits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenHabitsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
