8812d7b155ea42c844a882a2e80bad6b4a71658c
andreitecsor <andrei.tecsor@gmail.com>
2020-11-21 18:11:48 +0200

    AppUser repository + service + controller

--- src/main/java/com/greenhabits/GreenHabitsApplication.java
+++ src/main/java/com/greenhabits/GreenHabitsApplication.java
@@ -44,34 +44,34 @@ public class GreenHabitsApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         //region Dummy data
-        java.util.Date date = new java.util.Date();
-        AppUser user1 = new AppUser("Andrei Tecsor","andrei@ie.ase.ro", Date.from(Instant.parse("2020-06-17T10:33:00Z")));
-        AppUser user2 = new AppUser("Andu_XD","andu@ie.ase.ro", Date.from(Instant.parse("2020-06-28T12:13:20Z")));
-        appUserService.create(user1);
-        appUserService.create(user2);
-
-        Follow follow = new Follow(Date.from(Instant.parse("2020-07-02T00:33:00Z")),user1,user2);
-        Follow followBack = new Follow(Date.from(Instant.parse("2020-07-02T01:33:00Z")),user2,user1);
-        followService.create(follow);
-        followService.create(followBack);
-
-        Challenge c1 = new Challenge("No car, health ++");
-        Challenge c2 = new Challenge("Less plastic, happier planet");
-        Challenge c3 = new Challenge("Recycle habit");
-        challengeService.create(c1);
-        challengeService.create(c2);
-        challengeService.create(c3);
-
-        Enrol e1 = new Enrol(Date.from(Instant.parse("2020-10-01T12:13:20Z")),user2,c1);
-        enrolService.create(e1);
-        Enrol e2 = new Enrol(date,user1,c1);
-        enrolService.create(e2);
-
-        Enrol e3 = new Enrol(Date.from(Instant.parse("2020-09-08T12:13:20Z")),user1,c3);
-        enrolService.create(e3);
-
-        Enrol e4 = new Enrol(date,user1,c2);
-        enrolService.create(e4);
+//        java.util.Date date = new java.util.Date();
+//        AppUser user1 = new AppUser("Andrei Tecsor","andrei@ie.ase.ro", Date.from(Instant.parse("2020-06-17T10:33:00Z")));
+//        AppUser user2 = new AppUser("Andu_XD","andu@ie.ase.ro", Date.from(Instant.parse("2020-06-28T12:13:20Z")));
+//        appUserService.create(user1);
+//        appUserService.create(user2);
+//
+//        Follow follow = new Follow(Date.from(Instant.parse("2020-07-02T00:33:00Z")),user1,user2);
+//        Follow followBack = new Follow(Date.from(Instant.parse("2020-07-02T01:33:00Z")),user2,user1);
+//        followService.create(follow);
+//        followService.create(followBack);
+//
+//        Challenge c1 = new Challenge("No car, health ++");
+//        Challenge c2 = new Challenge("Less plastic, happier planet");
+//        Challenge c3 = new Challenge("Recycle habit");
+//        challengeService.create(c1);
+//        challengeService.create(c2);
+//        challengeService.create(c3);
+//
+//        Enrol e1 = new Enrol(Date.from(Instant.parse("2020-10-01T12:13:20Z")),user2,c1);
+//        enrolService.create(e1);
+//        Enrol e2 = new Enrol(date,user1,c1);
+//        enrolService.create(e2);
+//
+//        Enrol e3 = new Enrol(Date.from(Instant.parse("2020-09-08T12:13:20Z")),user1,c3);
+//        enrolService.create(e3);
+//
+//        Enrol e4 = new Enrol(date,user1,c2);
+//        enrolService.create(e4);
         //endregion Dummy Data
     }
 }
--- src/main/java/com/greenhabits/service/AppUserService.java
+++ src/main/java/com/greenhabits/service/AppUserService.java
@@ -6,18 +6,59 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AppUserService {
     @Autowired
     private AppUserRepository repository;
 
-    public List<AppUser> getAll(){
+    public AppUser create(AppUser appUser) {
+        if (appUser == null) {
+            return null;
+        }
+        return repository.save(appUser);
+    }
+
+    public List<AppUser> getAll() {
         return repository.findAll();
     }
 
-    public void create(AppUser appUser){
-        repository.save(appUser);
+    public AppUser getById(Long id) {
+        Optional<AppUser> result = repository.findById(id);
+        if (!result.isPresent()) {
+            return null;
+        }
+        return result.get();
     }
+
+    public AppUser update(AppUser user) {
+        if (user == null || user.getId() == null) {
+            return null;
+        }
+        Optional<AppUser> persistedUser = repository.findById(user.getId());
+        if (!persistedUser.isPresent()) {
+            return null;
+        }
+
+        AppUser userToUpdate = persistedUser.get();
+
+        userToUpdate.setName(user.getName());
+        userToUpdate.setCreatedAt(user.getCreatedAt());
+        userToUpdate.setEmail(user.getEmail());
+        userToUpdate.setChallenges(user.getChallenges());
+        return repository.save(userToUpdate);
+    }
+
+    public AppUser delete(Long id) {
+        Optional<AppUser> persistedUser = repository.findById(id);
+        if (!persistedUser.isPresent()) {
+            return null;
+        }
+        AppUser deletedUser = persistedUser.get();
+        repository.deleteById(id);
+        return deletedUser;
+    }
+
 
 }
