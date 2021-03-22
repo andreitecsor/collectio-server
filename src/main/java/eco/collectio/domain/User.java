b0626b6b31b86cdd04de1df44b10be4bb7965fd4
andreitecsor <andrei.tecsor@gmail.com>
2021-03-23 00:31:50 +0200

    fixed minor bug on creating user/challenge

--- src/main/java/eco/collectio/domain/User.java
+++ src/main/java/eco/collectio/domain/User.java
@@ -5,8 +5,10 @@ import org.neo4j.ogm.annotation.Id;
 import org.neo4j.ogm.annotation.NodeEntity;
 import org.neo4j.ogm.annotation.Property;
 
+import java.io.Serializable;
+
 @NodeEntity
-public class User {
+public class User implements Serializable {
     @Id
     @GeneratedValue
     private Long id;
@@ -14,7 +16,8 @@ public class User {
     @Property(name = "name")
     private String name;
 
-    public User( String name) {
+    public User(Long id, String name) {
+        this.id = id;
         this.name = name;
     }
 
