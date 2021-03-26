4f688b4dad9b112946618633370bcba8594eabf9
andreitecsor <andrei.tecsor@gmail.com>
2021-03-26 17:47:59 +0200

    Logger added

--- src/main/java/eco/collectio/service/JoinService.java
+++ src/main/java/eco/collectio/service/JoinService.java
@@ -1,9 +1,12 @@
 package eco.collectio.service;
 
+import eco.collectio.controller.ChallengeController;
 import eco.collectio.domain.Challenge;
 import eco.collectio.domain.Join;
 import eco.collectio.domain.User;
 import eco.collectio.repository.JoinRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -17,7 +20,7 @@ public class JoinService {
     private final UserService userService;
     private final ChallengeService challengeService;
 
-    //TODO: Add Logger
+    private Logger logger = LoggerFactory.getLogger(JoinService.class);
 
     public JoinService(JoinRepository joinRepository, UserService userService, ChallengeService challengeService) {
         this.joinRepository = joinRepository;
@@ -44,7 +47,7 @@ public class JoinService {
             Optional<User> persistedUser = userService.getById(userId);
             Optional<Challenge> persistedChallenge = challengeService.getById(challengeId);
             if (!persistedUser.isPresent() || !persistedChallenge.isPresent()) {
-                System.err.println("user or challenge does not exists");
+                logger.error("user(id=" + userId+ ") or challenge(id=" + challengeId+ ") does not exists");
                 return null;
             }
             Join join = new Join(LocalDate.now(), persistedUser.get(), persistedChallenge.get());
@@ -54,7 +57,7 @@ public class JoinService {
             result.restartChallenge();
             return joinRepository.save(result);
         }
-        System.err.println(result.toString() + " is still active");
+        logger.error(result.toString() + " is still active");
         return null;
     }
 
@@ -72,9 +75,6 @@ public class JoinService {
         if (result == null || result.getEndedAt() != null) {
             return null;
         }
-        //TODO:
-        // -check if he is in trust days, if yes return null
-        // -check if he is in check days, if yes update lastChecked = lastChecked + 1;
         int daysBetween = (int) ChronoUnit.DAYS.between(result.getLastChecked(), LocalDate.now());
         if (daysBetween < 4) {
             return null; //trust days period -> you cant check now
