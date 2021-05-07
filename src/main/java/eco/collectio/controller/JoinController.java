12ead39d2c978978d64f46c6a9945b6cf29c952b
andreitecsor <andrei.tecsor@gmail.com>
2021-05-07 14:44:54 +0300

    Adding CORS

--- src/main/java/eco/collectio/controller/JoinController.java
+++ src/main/java/eco/collectio/controller/JoinController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/joined")
 public class JoinController {
