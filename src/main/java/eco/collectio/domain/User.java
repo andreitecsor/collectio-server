ca2229ea0ef9c6275357fa69bb45cb6d22f6cc69
andreitecsor <andrei.tecsor@gmail.com>
2021-05-07 14:47:44 +0300

    changing name to displayName

--- src/main/java/eco/collectio/domain/User.java
+++ src/main/java/eco/collectio/domain/User.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 import org.neo4j.ogm.annotation.GeneratedValue;
 import org.neo4j.ogm.annotation.Id;
 import org.neo4j.ogm.annotation.NodeEntity;
-import org.neo4j.ogm.annotation.Property;
 
 import java.io.Serializable;
 
@@ -19,8 +18,7 @@ public class User implements Serializable {
     @GeneratedValue
     private Long id;
 
-    @Property(name = "displayName")
-    private String name;
+    private String displayName;
 
     private String email;
 
